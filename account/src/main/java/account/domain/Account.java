26ab1eaa57b16b43e7f41bdc3fd3249f2475c2c5
Marc <muilpp@gmail.com>
2020-06-14 11:16:19 +0200

    Added Account entity

--- account/src/main/java/account/domain/Account.java
+++ account/src/main/java/account/domain/Account.java
@@ -1,4 +1,34 @@
 package account.domain;
 
 public class Account {
+
+    private AccountId accountId;
+    private Money baselineBalance;
+    private ActivityWindow activityWindow;
+
+    public Money calculateBalance() {
+        return Mondey.add(this.baselineBalance, this.activityWindow.calculateBalance(this.id));
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+        if (!mayWithdraw(money)) {
+            return false;
+        }
+
+        Activity withdrawal = new Activity(this.id, targetAccountId, LocalTime.now, money);
+        this.activityWindow.addActivity(withdrawal);
+
+        return true;
+    }
+
+    private boolean mayWithdraw(Money money) {
+        return Money.add(this.calculateBalance(), money.negate()).isPositive();
+    }
+
+    public boolean deposit(Money money, AccountId sourceAccountId) {
+        Activity deposit = new Activity(this.id, sourceAccountId, LocalDateTime.now(), money);
+        this.activityWindow.addActivity(deposit);
+
+        return true;
+    }
 }
