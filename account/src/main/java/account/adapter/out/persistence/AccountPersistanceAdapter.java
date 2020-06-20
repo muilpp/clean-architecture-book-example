eba708844b661887670f5363fd0fdc068d4ff680
Marc <muilpp@gmail.com>
2020-06-20 23:33:05 +0200

    Added account persistance adapter

--- account/src/main/java/account/adapter/out/persistence/AccountPersistanceAdapter.java
+++ account/src/main/java/account/adapter/out/persistence/AccountPersistanceAdapter.java
@@ -2,8 +2,14 @@ package account.adapter.out.persistence;
 
 import account.application.port.out.LoadAccountPort;
 import account.application.port.out.UpdateAccountStatePort;
+import account.domain.Account;
+import account.domain.Activity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
@@ -11,7 +17,27 @@ public class AccountPersistanceAdapter implements LoadAccountPort, UpdateAccount
 
     private final AccountRepository accountRepository;
     private final ActivityRepository activityRepository;
-    private final AccountManager accountManager;
+    private final AccountMapper accountMapper;
 
-    
+    public Account loadAccount(AccountId accountId, LocalDateTime baselineDate) {
+        AccountJpaEntity account = accountRepository.findById(accountId.getValue()).orElseThrow(EntityNotFoundException::new);
+
+        List<ActivityJpaEntity> activities = activityRepository.findByOwnerSince(accountId.getValue(), baselineDate);
+        Long withdrawalBalance = orZero(activityRepository.getWithdrawalBalanceUntil());
+        Long depositBalance = orZero(activityRepository.getDepositBalanceUntil());
+
+        return accountMapper.mapToDomainEntity(account, activities, withdrawalBalance, depositBalance);
+    }
+
+    private Long orZero(Long value) {
+        return value == null ? 0L : value;
+    }
+
+    private void updateActivities(Account account) {
+        for (Activity activity : account.getActivityWindow().getActivities()) {
+            if (activity.getId() == null) {
+                activityRepository.save(accountMapper.mapToJpaEntity(activity));
+            }
+        }
+    }
 }
